70f666cc51c12b65b1821ccae2da86f8b05f48c4
Linkolnn <rozarioa1974@gmail.com>
2025-02-20 18:13:55 +0500

    deploy №11.3(edit footer btn text)

--- services/data.js
+++ services/data.js
@@ -24,7 +24,7 @@ const data = {
             footer: [
                 {
                     url: "/#more",
-                    text: 'О нас'
+                    text: 'О проекте'
                 }
             ],
             header: []
